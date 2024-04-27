b5b9320afa1ec42735dd38ed226745537f4d7fc2
diegoam11 <diegoalvarezmore@gmail.com>
2024-04-27 17:52:10 -0500

    init

---
+++ src/main/java/com/diegodev/springldapauth/config/SecurityConfig.java
@@ -0,0 +1,53 @@
+package com.diegodev.springldapauth.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.ldap.core.support.BaseLdapPathContextSource;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.ldap.LdapBindAuthenticationManagerFactory;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests(
+                authorize -> authorize
+                        .anyRequest()
+                        .fullyAuthenticated())
+                .formLogin(Customizer.withDefaults());
+
+        return http.build();
+    }
+
+    // this code is unnecessary if you configure ldap in our application properties
+
+    /*
+        @Bean
+        public LdapTemplate ldapTemplate() {
+            return new LdapTemplate(contextSource());
+        }
+
+        @Bean
+        public LdapContextSource contextSource() {
+            LdapContextSource ldapContextSource = new LdapContextSource();
+            ldapContextSource.setUrl("ldap://localhost:10389");
+            ldapContextSource.setUserDn("uid=admin,ou=system");
+            ldapContextSource.setPassword("secret");
+            return ldapContextSource;
+        }
+    */
+
+    @Bean
+    AuthenticationManager authManager(BaseLdapPathContextSource source) {
+        LdapBindAuthenticationManagerFactory factory = new LdapBindAuthenticationManagerFactory(source);
+        factory.setUserDnPatterns("cn={0},ou=users,ou=system");
+        return factory.createAuthenticationManager();
+    }
+
+}
---
+++ src/test/java/com/diegodev/springldapauth/SpringLdapAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diegodev.springldapauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLdapAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
