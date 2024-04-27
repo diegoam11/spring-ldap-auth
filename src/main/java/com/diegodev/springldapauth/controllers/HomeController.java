25acc563a081e3e74de354f23451128388d9ad44
diegoam11 <diegoalvarezmore@gmail.com>
2024-04-27 18:14:21 -0500

    added method controller to get user details

--- src/main/java/com/diegodev/springldapauth/controllers/HomeController.java
+++ src/main/java/com/diegodev/springldapauth/controllers/HomeController.java
@@ -1,5 +1,7 @@
 package com.diegodev.springldapauth.controllers;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +10,13 @@ public class HomeController {
     @GetMapping("/")
     public String home() {
         return "Welcome to Spring LDAP Auth";
+    }
+
+    @GetMapping("/getUserDetails")
+    public String getUserDetails(Authentication authentication) {
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        boolean accountNonExpired = userDetails.isAccountNonExpired();
+        return "Username: " + userDetails.getUsername() +
+                "\n Account Non Expired: " + accountNonExpired;
     }
 }
